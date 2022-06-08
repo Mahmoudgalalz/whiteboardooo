9726f9524f6682e6771e392da3940bb5f72e2612
Mahmoud Galal <ssss.aaa2002@gmail.com>
2022-06-08 17:44:48 +0200

    #bug: touchable devices support

--- app.js
+++ app.js
@@ -58,7 +58,8 @@ function eraserPanel(){
 
 //clearing the whole whiteboard
 document.getElementById('delete').addEventListener('click',()=>{
-    ctx.clearRect(0,0,canvas.width,canvas.height);
+    
+    canvas.width=canvas.width;
 })
 
 // rest on draw
@@ -104,8 +105,51 @@ window.addEventListener('load',()=>{
     canvas.addEventListener('mouseup',finishedPostion)
     canvas.addEventListener('mousemove',draw)
 
+    
     // touchable devices listener
-    canvas.addEventListener('touchstart',startPostion)
-    canvas.addEventListener('touchend',finishedPostion)
-    canvas.addEventListener('touchmove',draw)
+    // Set up touch events for mobile, etc
+    canvas.addEventListener("touchstart", function (e) {
+    mousePos = getTouchPos(canvas, e);
+    let touch = e.touches[0];
+    let mouseEvent = new MouseEvent("mousedown", {
+        clientX: touch.clientX,
+        clientY: touch.clientY
+        });
+    canvas.dispatchEvent(mouseEvent);
+        }, false);
+    
+    
+    canvas.addEventListener("touchend", function (e) {
+    let mouseEvent = new MouseEvent("mouseup", {});
+    canvas.dispatchEvent(mouseEvent);
+    }, false);
+
+
+    canvas.addEventListener("touchmove", function (e) {
+    let touch = e.touches[0];
+    let mouseEvent = new MouseEvent("mousemove", {
+    clientX: touch.clientX,
+    clientY: touch.clientY
+        });
+    canvas.dispatchEvent(mouseEvent);
+        }, false);
+
+// Get the position of a touch relative to the canvas
+    function getTouchPos(canvasDom, touchEvent) {
+    let rect = canvasDom.getBoundingClientRect();
+    return {
+    x: touchEvent.touches[0].clientX - rect.left,
+    y: touchEvent.touches[0].clientY - rect.top
+        };
+    }
+
+    canvas.addEventListener('touchstart',startPostion,(event)=>{
+        event.preventDefault();
+    })
+    canvas.addEventListener('touchend',finishedPostion,(event)=>{
+        event.preventDefault();
+    })
+    canvas.addEventListener('touchmove',draw,(event)=>{
+        event.preventDefault();
+    })
 })
